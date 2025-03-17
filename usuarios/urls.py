ba5cfcc942e909a37630d0a9b0edd58f72d5dfaf
carlosrosen <carloseduardo.paiva48@gmail.com>
2025-03-17 19:14:04 -0300

    Função de cadastro completo (Com a participação do Davi)

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 # As URLs de Login e Cadastro
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('esqueci/', views.esqueci, name='esqueci')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,35 @@
+from django.http.response import  HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-#Função de Cadastro
+#Função para cadastrar Usuarios
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
-#Função de Login
+        #Filtra se já existe um e-mail parecido no banco de dados, para evitar cadastro repetido
+        user = User.objects.filter(email=email).first()
+
+        #Caso o e-mail exista exibira uma mensagem
+        if user:
+            return HttpResponse('Já existe um usuário com esse e-mail')
+
+        # Chamando a função de criar usuário do Django
+        user = User.objects.create_user(username=usuario, email=email, password=senha)
+
+        # Salva as informações do usuario no banco de dados
+        user.save()
+
+        return HttpResponse("Usuário cadastrado com sucesso!")
+
+#Função de requisitar o site de Login
 def login(request):
     return render(request, 'login.html')
+
+#Função de requisitar o site de EsqueciMinhaSenha
+def esqueci(request):
+    return render(request, 'esqueci.html')
