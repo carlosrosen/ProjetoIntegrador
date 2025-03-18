621a17579ac9d917f74b8ad69cad658747aa0daf
carlosrosen <carloseduardo.paiva48@gmail.com>
2025-03-17 23:02:18 -0300

    Atulização no cadastro e login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,34 +1,45 @@
-from django.http.response import  HttpResponse
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
 
-#Função para cadastrar Usuarios
+#Função para cadastrar novos usuarios
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     else:
-        usuario = request.POST.get('usuario')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
+        usuario = request.POST.get('usuario', '').strip()
+        email = request.POST.get('email', '').strip()
+        senha = request.POST.get('senha', '')
 
-        #Filtra se já existe um e-mail parecido no banco de dados, para evitar cadastro repetido
-        user = User.objects.filter(email=email).first()
+        #Filtra se já existe um e-mail ou usuario parecidos no banco de dados, para evitar cadastros repetidos.
+        user = User.objects.filter(Q(username=usuario) | Q(email=email)).first()
 
-        #Caso o e-mail exista exibira uma mensagem
+        #Caso o e-mail ou usuario exista
         if user:
-            return HttpResponse('Já existe um usuário com esse e-mail')
+            return HttpResponse('Já existe um usuário com esse usuário ou email')
 
         # Chamando a função de criar usuário do Django
         user = User.objects.create_user(username=usuario, email=email, password=senha)
 
-        # Salva as informações do usuario no banco de dados
-        user.save()
-
         return HttpResponse("Usuário cadastrado com sucesso!")
 
-#Função de requisitar o site de Login
+#Função de Login
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        usuario = request.POST.get('usuario', '').strip()
+        senha = request.POST.get('senha')
+
+        #Autentica se o email e a senha são compativeís
+        user = authenticate(username=usuario, password=senha)
+
+        if user:
+            return HttpResponse("Usuário logado com sucesso!")
+        else:
+            return HttpResponse("Usuário ou senha errada")
 
 #Função de requisitar o site de EsqueciMinhaSenha
 def esqueci(request):
