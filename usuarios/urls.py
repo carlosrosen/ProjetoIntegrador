ac2cd9ff446b3089b5aee37b159319f151803b4b
Carlos <carloseduardo.paiva48@gmail.com>
2025-04-13 22:22:25 -0300

    Teste com o Davi

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views # Importei essa biblioteca para utilizar as views do Django de auth
 from . import views
 
 app_name = 'usuario'
@@ -8,6 +9,9 @@ urlpatterns = [
     path('cadastro/', views.cadastrar, name='cadastro'),
     path('login/', views.logar, name='login'),
     path('logout/', views.deslogar, name='logout'),
-    path('esqueci/', views.esqueci, name='esqueci'),
+    path('resetar-senha/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('resetar-senha/enviado/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('resetar-senha/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('resetar-senha/concluido/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
